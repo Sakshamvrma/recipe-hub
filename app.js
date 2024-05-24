7c5860155797d4cc3b381fcb5ee1e42ab7f925fb
[Saksham] <22bma033@nith.ac.in>
2024-05-24 18:09:30 +0530

    Added recipe routes and controllers

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
 const express=require('express');
 const app=express();
 const path = require('path');
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
-// const mongoSanitize = require('express-mongo-sanitize');
-// const xss = require('xss-clean');
-// const hpp = require('hpp');
-// const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
-// const compression = require('compression');
-// const cors = require('cors');
+const recipeRouter = require('./routes/recipeRoutes');
+const userRouter = require('./routes/userRoutes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const morgan = require('morgan');
 
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use(express.static(__dirname + 'public'));
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
-app.get('/',(req,res)=>{
-  res.send("Hii there")
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
+//Routes
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/recipes', recipeRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 module.exports=app;
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ connect(DB,{}
 then(()=>{
   console.log('Database connected less Gooo!!!!!!!')
 });
+app.get('/',(req,res)=>{
+  res.send('Hello from the server side');
+});
 const port=process.env.PORT || 3000;
 const server=app.listen(port,()=>{
   console.log(`Server is running on port ${port}......`)
