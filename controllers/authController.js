5b5163613840daf9d1ac351320621e151c2e4a02
[Saksham] <22bma033@nith.ac.in>
2024-05-27 16:56:48 +0530

    Added a authController file. Got a few more issues to deal with

---
+++ controllers/authController.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const catchAsync = require("../utils/catchAsync.js");
+const appError=require('../utils/appError.js')
+const jwt=require('jsonwebtoken');
+const User=require('../models/userModel.js')
+
+exports.signUp=catchAsync(async(req,res,next)=>{
+  const user=await User.create(req.body);
+  const token=jwt.createToken({id:user._id});
+  res.status(201).json({
+    status:'success',
+    token,
+    data:{
+      user
+    }
+  })
+})
+
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,29 +1,28 @@
-const express=require('express');
-const catchAsync=require('../utils/catchAsync');
-const User=require('../models/userModel');
-const AppError=require('../utils/appError');
+const express = require("express");
+const catchAsync = require("../utils/catchAsync.js");
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 
-exports.createUser=catchAsync(async(req,res)=>{
-  const user =await User.create(req.body);
+exports.createUser = catchAsync(async (req, res) => {
+  const user = await User.create(req.body);
   res.status(201).json({
-    status:'success',
-    data:user
-    
+    status: "success",
+    data: user,
   });
 });
-exports.getAllUsers=catchAsync(async(req,res,next)=>{
-  const users=await User.find();
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
   res.status(200).json({
-    status:'success',
-    results:users.length,
-     data:users
-  })
+    status: "success",
+    results: users.length,
+    data: users,
+  });
 });
-exports.getUser=catchAsync(async(req,res,next)=>{
-  const user=await User.findById(req.params.id);
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
   res.status(201).json({
-    status:'success',
-    data:user,
-    
-  })
-})
+    status: "success",
+    data: user,
+  });
+});
